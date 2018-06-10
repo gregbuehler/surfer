891707c097b348bb03fc5e0cf03260b12d1a7d92
Greg Buehler <gregory.r.buehler@gmail.com>
2018-06-09 22:16:42 -0400

    final updates to setup script

--- setup_directories.sh
+++ setup_directories.sh
@@ -8,10 +8,12 @@ group=surfer
 mode=0775
 
 # configs
+install -o $owner -g $group -m $mode -d $CONFIG
 install -o $owner -g $group -m $mode -d $CONFIG/{traefik,plex,nzbget,sonarr,radarr}
 install -o $owner -g $group -m $mode -d $DOWNLOAD
 install -o $owner -g $group -m $mode -d $DATA
 
+cp ./traefik.toml $CONFIG/traefik/
 touch $CONFIG/traefik/acme.json
 chmod 600 $CONFIG/traefik/acme.json
 
